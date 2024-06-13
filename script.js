49b9944802f099772ba4ebd3944c5c4617b5021e
aryan22csu031 <aryan22csu031@ncuindia.edu>
2024-06-14 03:15:21 +0530

    added new project

---
+++ script.js
@@ -0,0 +1,26 @@
+const logNav = document.querySelector(".log-nav");
+const profile = document.querySelector(".account");
+var flag = 0;
+profile.addEventListener("mouseenter",()=>{
+    if(flag==0){
+        logNav.style.display = "flex";
+        flag = 1;
+    }else{
+        profile.addEventListener("mouseleave",()=>{
+            logNav.style.display = "none";
+            flag = 0;
+        })
+    }
+})
+profile.addEventListener("mouseleave",()=>{
+    logNav.style.display = "none";
+})
+var typed = new Typed(".welcome-effect",{
+    strings:[" welcomes you aboard !!"],
+    typeSpeed:90,
+    backSpeed:90,
+    backDelay:1250,
+    startDelay:500,
+    cursorChar:"!",
+    loop:true
+});
